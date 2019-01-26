060937cfd60815f8641056e274ef1cc44f98b3d6
Anatoliy Korovin <antkorwin@gmail.com>
2019-01-27 09:55:25 +1000

    add PostgreSQL and RabbitMq annotations

---
+++ spring-test-postgres/src/test/java/com/jupiter/tools/spring/test/postgres/annotation/meta/EnablePostgresIntegrationTestTest.java
@@ -0,0 +1,30 @@
+package com.jupiter.tools.spring.test.postgres.annotation.meta;
+
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.jupiter.tools.spring.test.postgres.TransactionalTestConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+
+/**
+ * Created on 23.01.2019.
+ * <p>
+ * TODO: replace on javadoc
+ *
+ * @author Korovin Anatoliy
+ */
+@EnablePostgresIntegrationTest
+@Import(TransactionalTestConfig.class)
+class EnablePostgresIntegrationTestTest {
+
+    @Autowired
+    private TransactionalTestConfig.TestService testService;
+
+    @Test
+    @DataSet(cleanBefore = true, cleanAfter = true)
+    @ExpectedDataSet(value = "/datasets/expected.json", ignoreCols = "ID")
+    void testCreate() {
+        testService.ok();
+    }
+}
---
+++ spring-test-rabbitmq/src/test/java/com/jupiter/tools/spring/test/rabbitmq/extension/EnableRabbitMqTestTest.java
@@ -0,0 +1,79 @@
+package com.jupiter.tools.spring.test.rabbitmq.extension;
+
+import com.jupiter.tools.spring.test.rabbitmq.annotation.EnableRabbitMqTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+/**
+ * Created on 23.01.2019.
+ * <p>
+ * TODO: replace on javadoc
+ *
+ * @author Korovin Anatoliy
+ */
+@EnableRabbitMqTest
+class EnableRabbitMqTestTest {
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    @Test
+    void testSend() {
+        // Arrange
+        // Act
+        amqpTemplate.convertAndSend("second-test-queue", "123");
+
+        await().atMost(3, TimeUnit.SECONDS)
+               .until(() -> TestConfig.events.size() > 0);
+
+        // Asserts
+        assertThat(TestConfig.events).containsOnly("123");
+    }
+
+    @TestConfiguration
+    public static class TestConfig {
+
+        public static List<String> events = new ArrayList<>();
+
+        @Bean
+        public RestTemplate restTemplate() {
+            return new RestTemplate();
+        }
+
+        @Bean
+        public Queue testQueue() {
+            return new Queue("second-test-queue");
+        }
+
+        @Component
+        @EnableRabbit
+        public class TestRabbitListener {
+
+            @RabbitListener(queues = "second-test-queue")
+            public void receive(String message) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                events.add(message);
+                System.out.println("!!!! " + message);
+            }
+        }
+    }
+}
