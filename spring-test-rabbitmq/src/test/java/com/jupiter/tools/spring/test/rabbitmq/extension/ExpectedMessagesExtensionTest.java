463b08197e21dfbb029321c9554e71a859724d5a
Anatoliy Korovin <antkorwin@gmail.com>
2019-03-25 22:19:10 +1000

    try to fix test in travis

--- spring-test-rabbitmq/src/test/java/com/jupiter/tools/spring/test/rabbitmq/extension/ExpectedMessagesExtensionTest.java
+++ spring-test-rabbitmq/src/test/java/com/jupiter/tools/spring/test/rabbitmq/extension/ExpectedMessagesExtensionTest.java
@@ -1,5 +1,6 @@
 package com.jupiter.tools.spring.test.rabbitmq.extension;
 
+import com.jupiter.tools.spring.test.rabbitmq.annotation.EnableRabbitMqTestContainers;
 import com.jupiter.tools.spring.test.rabbitmq.annotation.ExpectedMessages;
 import com.jupiter.tools.spring.test.rabbitmq.annotation.meta.EnableRabbitMqTest;
 import com.jupiter.tools.spring.test.rabbitmq.extension.pojo.Bar;
@@ -10,8 +11,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * Created on 25.03.2019.
@@ -20,7 +23,10 @@ import org.springframework.context.annotation.Bean;
  *
  * @author Korovin Anatoliy
  */
-@EnableRabbitMqTest
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@EnableRabbitMqTestContainers
+@ExtendWith(RabbitMqMessageExtension.class)
 @ExtendWith(ExpectedMessagesExtension.class)
 class ExpectedMessagesExtensionTest {
 
