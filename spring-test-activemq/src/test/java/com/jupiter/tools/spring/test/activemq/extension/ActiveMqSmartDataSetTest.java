11e723085d4f423bce419971d4fa77b8c1cde49c
Anatoliy Korovin <antkorwin@gmail.com>
2020-06-10 08:36:17 +1000

    activemq with smart matching

---
+++ spring-test-activemq/src/test/java/com/jupiter/tools/spring/test/activemq/extension/ActiveMqSmartDataSetTest.java
@@ -0,0 +1,72 @@
+package com.jupiter.tools.spring.test.activemq.extension;
+
+import java.util.Date;
+
+import com.jupiter.tools.spring.test.activemq.annotation.ExpectedMessages;
+import com.jupiter.tools.spring.test.activemq.annotation.meta.EnableActiveMqTest;
+import com.jupiter.tools.spring.test.activemq.extension.expected.Foo;
+import com.jupiter.tools.spring.test.activemq.extension.expected.FooWithBar;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created on 25.03.2019.
+ *
+ * @author Korovin Anatoliy
+ */
+@EnableActiveMqTest
+public class ActiveMqSmartDataSetTest {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Test
+    @ExpectedMessages(queue = "test-queue", messagesFile = "/datasets/expected_messages.json")
+    void testSendListOfMessages() {
+        jmsTemplate.convertAndSend("test-queue", new Foo("123"));
+        jmsTemplate.convertAndSend("test-queue", new Foo("456"));
+        jmsTemplate.convertAndSend("test-queue", new Foo("789"));
+    }
+
+    @Test
+    @ExpectedMessages(queue = "test-queue", messagesFile = "/datasets/expected_messages_with_js.json")
+    void testExpectedDataSetWithJavaScript() {
+        jmsTemplate.convertAndSend("test-queue", new Foo( String.valueOf(1+2+3+4+5)));
+    }
+
+    @Test
+    @ExpectedMessages(queue = "test-queue", messagesFile = "/datasets/expected_messages_with_date.json")
+    void testExpectedWithDate() {
+        // Arrange
+        FooWithBar fooWithBar = FooWithBar.builder()
+                                          // NOW
+                                          .time(new Date())
+                                          .build();
+        // Act
+        jmsTemplate.convertAndSend("test-queue", fooWithBar);
+    }
+
+    @TestConfiguration
+    public static class TestConfig {
+        @Bean
+        public Queue testQueue() {
+            return new Queue("test-queue");
+        }
+
+        @Bean // Serialize message content to json using TextMessage
+        public MessageConverter jacksonJmsMessageConverter() {
+            MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+            converter.setTargetType(MessageType.TEXT);
+            converter.setTypeIdPropertyName("_type");
+            return converter;
+        }
+    }
+}
---
+++ spring-test-activemq/src/test/java/com/jupiter/tools/spring/test/activemq/extension/expected/FooWithBar.java
@@ -0,0 +1,28 @@
+package com.jupiter.tools.spring.test.activemq.extension.expected;
+
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+/**
+ * Created on 07.02.2019
+ *
+ * @author Korovin Anatoliy
+ */
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FooWithBar {
+	Foo foo;
+	Bar bar;
+	FooWithBar child;
+	Date time;
+}
